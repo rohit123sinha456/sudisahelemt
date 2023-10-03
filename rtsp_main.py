723d845681f72226ea029d611c729c61598c2d62
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2023-10-03 12:55:18 +0530

    Add files via upload

---
+++ rtsp_main.py
@@ -0,0 +1,11 @@
+from infer import Infer
+from api_post import API
+from rtsp import RTSP
+import cv2
+import time
+if __name__=="__main__":
+    inferob = Infer()
+    api = API()
+    rtspob = RTSP(inferob,api,"rtsp://admin:Sheraton123@10.111.111.106:554/Streaming/Channels/501/")
+    rtspob.run_threads()
+    
