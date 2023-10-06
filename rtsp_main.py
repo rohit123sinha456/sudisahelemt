1ac106bd5a5d17579b1cccc488d527023989f462
Rohit <rohit@gmail.com>
2023-10-06 15:29:01 +0530

    Debug Multi Threads

--- rtsp_main.py
+++ rtsp_main.py
@@ -20,18 +20,28 @@ if __name__=="__main__":
         for rtsp_object in rtsp_object_list:
             print("Queue Thread Status :- ", rtsp_object.QueueThread.is_alive())
             if(rtsp_object.QueueThread.is_alive() == False):
+                print("killing Dequeue")
                 rtsp_object.DequeueThread.join()
+                print("GC Object")
                 rtsp_object_list.remove(rtsp_object)
+                print("Creating New object")
                 rtspob = RTSP(inferob,api,camera_config)
+                print("Running Threads")
                 rtspob.run_threads()
+                print("appending object")
                 rtsp_object_list.append(rtspob)
 
             print("Dequeue Thread Status :- ", rtsp_object.DequeueThread.is_alive())
             if(rtsp_object.DequeueThread.is_alive() == False):
+                print("killing EnQueue")
                 rtsp_object.QueueThread.join()
+                print("GC Object")
                 rtsp_object_list.remove(rtsp_object)
+                print("Creating New object")
                 rtspob = RTSP(inferob,api,camera_config)
+                print("Running Threads")
                 rtspob.run_threads()
+                print("appending object")
                 rtsp_object_list.append(rtspob)
                 
         time.sleep(60)
